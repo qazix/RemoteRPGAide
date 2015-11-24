2b15182d705747bdaede49f4c93e9e02e24689f1
Aaron.Hanich@gmail.com <aaron.hanich@gmail.com>
2015-11-23 22:53:52 -0700

    First commit
    n/a

---
+++ RemoteRPGAideServer/src/main/java/com/RRPGAServer/controller/firstController.java
@@ -0,0 +1,13 @@
+package com.RRPGAServer.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v0")
+public class firstController {
+	@RequestMapping("/hello")
+	public String hello() {
+		return "Hello";
+	}
+}
